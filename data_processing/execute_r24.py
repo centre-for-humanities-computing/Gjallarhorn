cbfb725e77374ad26dea7741326364d3989df7a3
arpelarpe <rasmus.arpe@gmail.com>
2022-05-24 10:27:45 +0200

    fix logfile

--- data_processing/execute_r24.py
+++ data_processing/execute_r24.py
@@ -17,9 +17,15 @@ for dir in all_dirs:
         os.makedirs(out_dir)
 
     in_dir = f"/work/data/p1-r24syv/files/24syv/{dir}"
+    log_file = f"{processes_output_dir}/{dir}.logfile"
+
+    # Create file
+    with open(log_file, "w", encoding="utf-8") as f:
+        pass
+
     process = Popen(
         [str(sys.executable)] + ['process_dir.py'] + ["--directory_in"] + [in_dir] + ["--directory_out"] + [out_dir],
-        stdout=f"{processes_output_dir}/{dir}.logfile"
+        stdout=log_file
     )
 
     jobs.append(process)
