969f7cc9bf8604d57051293fe52e3bb33de6d1f0
arpelarpe <rasmus.arpe@gmail.com>
2022-05-24 10:22:41 +0200

    log output

--- data_processing/execute_r24.py
+++ data_processing/execute_r24.py
@@ -5,6 +5,7 @@ from subprocess import Popen
 jobs = []
 
 all_dirs = os.listdir("/work/data/p1-r24syv/files/24syv")
+processes_output_dir = "./processes_output"
 
 for dir in all_dirs:
     if dir == "2011-11":
@@ -15,7 +16,8 @@ for dir in all_dirs:
 
     in_dir = f"/work/data/p1-r24syv/files/24syv/{dir}"
     process = Popen(
-        [str(sys.executable)] + ['process_dir.py'] + ["--directory_in"] + [in_dir] + ["--directory_out"] + [out_dir]
+        [str(sys.executable)] + ['process_dir.py'] + ["--directory_in"] + [in_dir] + ["--directory_out"] + [out_dir],
+        stdout=f"{processes_output_dir}/{dir}.logfile"
     )
 
     jobs.append(process)
