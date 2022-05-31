e7cdaa0d036c4780d8fcee6c2063eccb098932e7
rasmus arpe fogh egebaek <rasmus@alvenir.ai>
2022-05-31 13:21:18 +0200

    Files for final creation

--- data_processing/create_final_tsv_file.py
+++ data_processing/create_final_tsv_file.py
@@ -2,17 +2,36 @@ import os
 
 def traverse_and_create_new_tsv_file(traverse_root_path: str, tsv_out_file: str):
     meta_data_files = []
+    counter = 0
+    print("Traversing")
+
     for root, subdirs, files in os.walk(traverse_root_path):
         for f in files:
             if f[-3:] == "tsv":
                 meta_data_files.append((root, f))
+                counter += 1
+            
+        if counter % 100 == 0:
+            print(counter)
 
+
+    print("Appending")
     all_lines_final = []
+    counter_last = 0
     for root, f in meta_data_files:
+        counter_last += 1
         with open(os.path.join(root, f), "r", encoding="utf-8") as f:
-            all_lines = f.read().split("\n")
-            all_lines_final += all_lines
+            pre_path = "/".join(str(f).split("/")[4:-1])
 
+            all_lines = f.read().split("\n")
+            for line in all_lines:
+                final_line = pre_path + "/" + line
+                all_lines_final.append(final_line)
+
+        if counter_last % 100 == 0:
+            print(f"{counter_last}/{len(meta_data_files)}")
+
+    print("Writing")
     with open(tsv_out_file, "w", encoding="utf-8") as f:
         f.write("\n".join(all_lines_final))
 
